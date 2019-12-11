530752fb2cd57696b914542d77e1f1702c238cee
unknown <wkwk2805@gmail.com>
2019-12-11 14:45:05 +0900

    화면 마진 추가

--- components/SearchList/ViewContent.js
+++ components/SearchList/ViewContent.js
@@ -3,7 +3,7 @@ import { Grid, Image } from "semantic-ui-react";
 import path from "path";
 
 const ViewContent = () => (
-  <Grid container columns={3}>
+  <Grid container columns={3} style={{ marginTop: "20px" }}>
     <Grid.Column>
       <Image src="../public/image.png" />
       안녕하세요
